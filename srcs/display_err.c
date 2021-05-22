578fc3b25d88d37d968fd0285b2806b4d05e2b21
Francisco Barros <fbarros@student.42lisboa.com>
2021-05-22 17:30:21 +0100

    linked list done

--- srcs/display_err.c
+++ srcs/display_err.c
@@ -1,39 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   display_err.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbarros <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/16 13:19:55 by fbarros           #+#    #+#             */
-/*   Updated: 2021/05/22 17:29:28 by fbarros          ###   ########.fr       */
+/*   Created: 2021/05/22 16:15:22 by fbarros           #+#    #+#             */
+/*   Updated: 2021/05/22 16:20:29 by fbarros          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
-#include <stdio.h>
 
-/*DEREFERENCE elements using *(int *)a.head->content */
-
-static void	del(void *content)
+void	display_err(void)
 {
-	if (!content)
-		return ;
-	content = NULL;
-}
-
-int	main(int argc, char **argv)
-{
-	int		n[argc - 1];
-	t_stack	a;
-	int		i;
-
-	i = 0;
-	while (++i < argc)
-		n[i - 1] = ft_atoi(argv[i]);
-	a.size = argc - 1;
-	while (i--)
-		ft_lstadd_front(&a.head, ft_lstnew(&n[i])); //ADDING BOTTOM UP
-	ft_lstclear(&a.head, del);
-	return (0);
+	ft_putstr_fd("Error\n", 2);
+	exit(1);
 }
