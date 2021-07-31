0af861422ef69350eeffb7ff3b03f69cea6dc53a
Francisco Barros <fbarros@student.42lisboa.com>
2021-07-31 16:32:10 +0100

    merge sort init

--- 2nd_impl/srcs/sort_array.c
+++ 2nd_impl/srcs/sort_array.c
@@ -12,29 +12,46 @@
 
 #include "../inc/push_swap.h"
 
-/*static int	*heap_sort(int *heap)
-{
+/*Best to copy outside function and merge list itself instead of allocating memory
+	Need to break into more functions*/
 
-}
-*/
-void	sort_array(int *lst, int len)
-/*Stuppidest shit ever
- Heap sort*/
+int	*merge(int *left, int *right, int q)
 {
-	(void)lst;
-	(void)len;
-/*	int	ret[len];
+	int	results[q * 2];
 	int	i;
 
 	i = 0;
-	ret[i] = lst[i];*/
-/*	while (lst[i])
+	while (results[i])
 	{
-		if ();
+		if (*left < *right)
+			results[i++] = *left++;
+		else
+			results[i++] = *right++;
 	}
-	return (heap_sort(ret));*/
-	return ;
+	return (results);
 }
 
+void	merge_sort(const int *a, int len, int *copy) //Not sure copy would work with recursion; 
+{
+	int	q;
 
+	q = len / 2; // middle
+	if ((sizeof(a) / sizeof(a[0]) < len)) //base case
+		return ; //return (results); (??)
+	merge_sort(a, q - 1, copy); //DIVIDE LEFT SIDE
+	merge_sort(a[q], q, copy); //DIVIDE RIGHT SIDE
+	copy = merge(a, a[q], q);
+//	return (copy); //MERGE BOTH SIDES (malloc and return results or return a)
+}
 
+int main(void)
+{
+	int array[] = {5, 3, 4, 1, 9, 6, 0, 2}; //TEST ODD NUMBERS
+	int len = sizeof(array) / sizeof(array[0]);
+	int copy[len];
+	int	i = -1;
+
+	merge_sort(array, len, copy);
+	while (++i < len)
+		printf("%d\t", array[i]);
+}
