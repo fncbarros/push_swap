f1776b01113ff7cc7a7963c5c3cc98466c41c211
fbarros <fbarros@student.42lisboa.com>
2021-08-11 16:35:41 +0100

    final

--- 2nd_impl/srcs/sort_array.c
+++ 2nd_impl/srcs/sort_array.c
@@ -6,7 +6,7 @@
 /*   By: fbarros <fbarros@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/27 23:33:02 by fbarros           #+#    #+#             */
-/*   Updated: 2021/07/27 23:33:05 by fbarros          ###   ########.fr       */
+/*   Updated: 2021/08/11 15:49:37 by fbarros          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,21 +56,3 @@ void	quicksort(int *a, int l, int r)
 	quicksort(a, l, i);
 	quicksort(a, i + 1, r + 1);
 }
-
-/*
-int main(void)
-{
-	int array[] = {9, -8, 7, -6, 5, 99, -4, 3, 0, 2, 1, 90001, 11}; //TEST ODD NUMBERS
-	int len = sizeof(array) / sizeof(array[0]);
-	int	i = -1;
-
-	while (++i < len)
-		printf("%d\t", array[i]);
-//	merge_sort(array, 0, len);
-	quicksort(array, 0, len); //or len - 1
-	i = -1;
-	printf("\nAfter merge: \nlen = %d\n", len);
-	while (++i < len)
-		printf("i%d\t%d\n",  i , array[i]);
-}
-*/
